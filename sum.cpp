918f9fc7afa35b05f093097879a79b6952cab0ae
Nihalk27 <nihalkondukwar@gmail.com>
2025-03-06 21:37:16 +0530

    The sum of two numbers

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two integers"<<endl;
+    cin>>a>>b;
+    int sum=0;
+    sum=a+b;
+    cout<<"The sum is "<<sum;
+    return 0;
+
+}
