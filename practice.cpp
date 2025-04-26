b299f1acab09330528349cbd6e8ecc3cc2979126
Nihalk27 <119446824+Nihalk27@users.noreply.github.com>
2025-04-26 13:51:05 +0530

    various type of practice on array

--- practice.cpp
+++ practice.cpp
@@ -2,28 +2,28 @@
 #include<iostream>
 using namespace std;
 int main(){
-  // int arr[5];
-  // { //calculating the sum of a all elementes in  array
-  //  cout<<"Enter the Element in array "<<endl;
-  // for(int i = 0 ; i < 5; i++){
-  //  cin>>arr[i];
-  // }
-  // int sum = 0;
-  // for(int i = 0; i< 5 ; i++){
-  //   sum = sum + arr[i];
-  // }
-  // cout<<"The sum is : "<<sum<<endl;
-  // }
-  // {
-  //   // to calculate the product of an given array 
-  //   int brr[] = {9,8,7,6,5,4,3};
-  //   int size = sizeof(brr)/4;
-  //   int product=1;
-  //   for (int i = 0 ; i< size ; i++){
-  //       product = product * brr[i];
-  //   }
-  //   cout<<"the Product is :"<<product<<endl;
-  // }
+  int arr[5];
+  { //calculating the sum of a all elementes in  array
+   cout<<"Enter the Element in array "<<endl;
+  for(int i = 0 ; i < 5; i++){
+   cin>>arr[i];
+  }
+  int sum = 0;
+  for(int i = 0; i< 5 ; i++){
+    sum = sum + arr[i];
+  }
+  cout<<"The sum is : "<<sum<<endl;
+  }
+  {
+    // to calculate the product of an given array 
+    int brr[] = {9,8,7,6,5,4,3};
+    int size = sizeof(brr)/4;
+    int product=1;
+    for (int i = 0 ; i< size ; i++){
+        product = product * brr[i];
+    }
+    cout<<"the Product is :"<<product<<endl;
+  }
   {
     // to find max in array without sorting it
      int crr[] = {91,845,2347,62,564,324,223};
